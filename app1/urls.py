639d55ef74d96a6c26b468cf4f454e7fae4c5721
Muzamil1920 <Muzamil1920@gmail.com>
2023-10-24 21:05:42 +0530

    footer-updated

--- app1/urls.py
+++ app1/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("" , views.index),
-    path("elements/" , views.elements, name="elements"),
-    path("generic/" , views.generic , name="generic")
+    path("/elements" , views.elements, name="elements"),
+    path("/generic" , views.generic , name="generic")
 ]
