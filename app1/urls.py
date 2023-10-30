ede8b2b12eff9a4b900171cdfd7cfc0a87a09848
Muzamil1920 <Muzamil1920@gmail.com>
2023-10-30 21:53:38 +0530

    models

--- app1/urls.py
+++ app1/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from app1 import views
 
 urlpatterns = [
     path("" , views.index),
-    path("/elements" , views.elements, name="elements"),
-    path("/generic" , views.generic , name="generic")
+    path("elements/" , views.elements, name="elements"),
+    path("generic/" , views.generic , name="generic")
 ]
